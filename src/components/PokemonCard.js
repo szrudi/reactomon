b6686e07caa6060b55dd4cd58f0bff8dfae46f8d
Szabó Rudi <rudi.szabo@codecool.com>
2021-01-14 12:36:49 +0100

    Use CardsContainer and Card in Pokemon List

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import {Card} from "../elements/Card";
 
 const PokemonCard = props => {
     return (
-        <Link to={`/pokemon/${props.pokemon.id}`}>
-            <div>{props.pokemon.name}</div>
-        </Link>
+        <Card small centered>
+            <Link to={`/pokemon/${props.pokemon.id}`}>
+                <div>{props.pokemon.name}</div>
+            </Link>
+        </Card>
     );
 }
 
--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -1,24 +1,26 @@
 import PokemonCard from "./PokemonCard";
 import {useHttp} from "../hooks/useHttp";
+import {useParams} from "react-router-dom";
+import {CardsContainer} from "../elements/CardsContainer";
 
 const PokemonList = props => {
-    const [data, isLoading] = useHttp('https://pokeapi.co/api/v2/pokemon');
-
-    let content = <p>Loading list...</p>;
-
-    if (data && !isLoading) {
-        let pokemonList = data.results.map(p => ({...p, id: p.url.match(/\/(\d+)\/$/)[1]}));
-        content = pokemonList.map(
-            pokemon => <PokemonCard pokemon={pokemon} key={pokemon.id}/>
+    let cards = null;
+    let [pokemonList] = useHttp('https://pokeapi.co/api/v2/pokemon');
+    if (pokemonList) {
+        pokemonList = pokemonList.results.map(p => ({...p, id: p.url.match(/\/(\d+)\/$/)[1]}));
+        cards = pokemonList.map(pokemon =>
+            <PokemonCard pokemon={pokemon} key={pokemon.id}/>
         );
     }
 
-    return (
-        <div>
-            <h1>List of pokemon</h1>
-            {content}
+    return <>
+        <h1>Pokemon list</h1>
+        <div className="content">
+            <CardsContainer>
+                {cards ? cards : <p>Loading list...</p>}
+            </CardsContainer>
         </div>
-    );
+    </>;
 }
 
 export default PokemonList;
