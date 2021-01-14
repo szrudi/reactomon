bac40714417575619120ce9e9e09fc33f63b8a87
Szabó Rudi <rudi.szabo@codecool.com>
2021-01-14 12:21:07 +0100

    Add basic error handling to useHttp hook

--- src/hooks/useHttp.js
+++ src/hooks/useHttp.js
@@ -8,12 +8,24 @@ export const useHttp = (url) => {
         setLoading(true);
         console.log("Sending HTTP request to: " + url);
         fetch(url)
-            .then(response => response.json())
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Failed to fetch.');
+                }
+                return response.json();
+            })
             .then(data => {
                 // console.log(data);
                 setData(data);
                 setLoading(false);
+            })
+            .catch(reason => {
+                console.log(reason);
+                setLoading(false);
             });
+        return () => {
+            setData(null);
+        }
     }, [url]);
 
     return [fetchedData, isLoading];
